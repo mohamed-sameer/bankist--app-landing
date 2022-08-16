a71d645bf61c06cf80e8824a2b2175686f39e04f
mohamed samir <mohamad.samir08@gmail.com>
2022-08-16 18:44:07 +0200

    add cookie message
    - create DOM element for the cookie message
    - add class to the created DOM element
    - add the cookie message DOM to the header
    - add click event on it to delete it
    - change class in css file from cookie-message to c-message to solve a conflict

--- script.js
+++ script.js
@@ -8,7 +8,8 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 
-const openModal = function () {
+const openModal = function (e) {
+  e.preventDefault();
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
 };
@@ -18,8 +19,7 @@ const closeModal = function () {
   overlay.classList.add('hidden');
 };
 
-for (let i = 0; i < btnsOpenModal.length; i++)
-  btnsOpenModal[i].addEventListener('click', openModal);
+btnsOpenModal.forEach(modal => modal.addEventListener('click', openModal));
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
@@ -29,3 +29,20 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+const header = document.querySelector('.header');
+const message = document.createElement('div');
+message.classList.add('c-message');
+// message.textContent = 'we use cookies to improve functionality and analytics';
+message.innerHTML =
+  'we use cookies to improve functionality and analytics <button class="btn btn--close-cookie">Got It!</button>';
+
+// header.prepend(message);
+// header.before(message);
+header.append(message);
+// header.append(message.cloneNode(true));
+document
+  .querySelector('.btn--close-cookie')
+  .addEventListener('click', function () {
+    message.remove();
+  });
